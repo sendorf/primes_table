536e9afa8c9ce4cc57d4dc07a4de07d60bd73a34
Sendorf <sendorf@gmail.com>
2015-06-16 18:17:20 +0200

    Created generate method to generate X prime numbers

--- lib/primes_table/primes_generator.rb
+++ lib/primes_table/primes_generator.rb
@@ -1,5 +1,23 @@
 module PrimesTable
 	class PrimesGenerator
+		def generate count
+			index = 2 # Current number to check
+			primes = [] # Acumulated primes so far
+			while primes.count < count
+				(primes << index) if prime_number? index
+				index += 1
+			end
+			return primes
+		end
 
+		protected
+
+		def prime_number? number
+			return false if number < 2
+	    (2..(number / 2)).each do |i|
+	      return false if number % i == 0
+	    end
+	    return true
+		end
 	end
 end
